010d2d1be830f9d3a309a317143ff07820ae9ff6
Ryan <mayerryan98@gmail.com>
2018-10-22 18:21:44 -0400

    GameSettings Class
    used to store the settings for the game

---
+++ GameSettings.java
@@ -0,0 +1,12 @@
+package project_aliens_mainmenu;
+
+/**
+ *
+ * @author Ryan
+ */
+public class GameSettings {
+    public int gameMapID; //int ID for game map selected
+    public int numGameWaves; //number of waves of aliens in the game
+    public int numStartingHumans; //number of human pawns at start of gane
+    public int numStartingAliens; //number of alien pawns at start of game
+}
